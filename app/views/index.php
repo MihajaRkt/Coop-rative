cce6623eed8307181bc09d4373cd094f076bf709
Tendry-Rlr <ralaiarimangatendry@gmail.com>
2025-12-12 15:53:28 +0300

    index modif

--- app/views/index.php
+++ app/views/index.php
@@ -1,24 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Coopérative - Accueil </title>
 </head>
+
 <body>
 
     <h1> Coopératives </h1>
 
     <h2> Entrez les informations nécessaires </h2>
 
-    <p> (Début) Date : <input type="date" name="d_debut"> Heure : <input type="time" name="h_debut"> </p>
-    <p> (Fin) Date : <input type="date" name="d_fin"> Heure : <input type="time" name="h_fin"> </p>    
-    <p> Recette : <input type="number" name="recette"> </p>
-    <p> Carburant : <input type="number" name="carburant" > </p>
-
-    <p> <input type="submit" value="Valider"> </p>
+    <form action="/insertion" method="post">
+        <p>Vehicule
+            <select name="vehicule">
+                <?php foreach ($listeVehicule as $l) { ?>
+                    <option value="<?= $l['idvehicule'] ?>"><?= $l['nom'] ?></option>
+                <?php } ?>
+            </select>
+        </p>
+        <p> (Début) Date : <input type="date" name="d_debut"> Heure : <input type="time" name="h_debut"> </p>
+        <p> (Fin) Date : <input type="date" name="d_fin"> Heure : <input type="time" name="h_fin"> </p>
+        <p> Recette : <input type="number" name="recette"> </p>
+        <p> Carburant : <input type="number" name="carburant"> </p>
+    
+        <p> <input type="submit" value="Valider"> </p>
+    </form>
 
     <a href="#"> Liste des coopératives</a>
 
 </body>
+
 </html>
